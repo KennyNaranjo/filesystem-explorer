70613173c1df171f58f98be377ddce8520bbaec8
Kenny <kenny.naranjo.castro@gmail.com>
2023-01-10 17:30:13 +0100

    bottons up

---
+++ fun.js
@@ -0,0 +1,12 @@
+
+
+const tdTittle = document.getElementById("table");
+tdTittle.addEventListener("click", checkClick);
+
+function checkClick(e){
+    if(e.target.matches("deleteRow")){
+        const tIndex = e.target.parentNode.parentNode.rowIndex
+        tdTittle.deleteRow(tIndex);
+    }
+
+}
