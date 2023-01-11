2a41e8350b32b31f7799a1a40d33322514b08148
Sergi <scsanz87@gmail.com>
2023-01-11 11:31:00 +0100

    php changes

--- php.php
+++ php.php
@@ -1,9 +1,31 @@
 <?php
-$fopen("index.html");
+print_r($_FILES);
+
+$name=$_FILES["Upload"]["name"];
 
 
+$content = $_FILES["Upload"]["tmp_name"];
 
 
+if(!file_exists("Upload")){
+    mkdir("Upload",0777,true);
+    if(file_exists("Upload")){
+        if(move_uploaded_file($content,"root/".$name)){
+            echo "saved";
+        }else{
+            echo"error";
+        }
 
+    }
 
+}else{
+    if(move_uploaded_file($content,"root/".$name)){
+        echo"saved";
+    } else{
+        echo"error";
+    }
+}
+if($name=="docx"){
+    move_uploaded_file($content,"root/documents/".$name);
+}
 ?>
